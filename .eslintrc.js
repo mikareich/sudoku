cf81385c53445594bc7cda5bda59ec8232b458a7
Mika Reich <mikareich0@gmail.com>
2021-07-19 12:55:46 +0200

    add sudoku interpreter

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+  },
+}
---
+++ src/Sudoku.ts
@@ -0,0 +1,104 @@
+type Unit = number[]
+
+/** Interprets and splits sudoku */
+class Sudoku {
+  /** Plain number sequence of the sudoku. Basically all sudoku rows side by side. */
+  public plain: Unit = []
+
+  /** Rows of sudoku as two-dimensional array */
+  public rows: Unit[] = []
+
+  /** Cols of sudoku as two-dimensional array */
+  public cols: Unit[] = []
+
+  /** Blocks of sudoku as two-dimensional array */
+  public blocks: Unit[] = []
+
+  /**
+   * Interprets and splits sudoku
+   * @param plain Plain number sequence of the sudoku. Basically all sudoku rows side by side.
+   */
+  constructor(plain: Unit) {
+    this.plain = plain
+
+    this.rows = Sudoku.formatToRows(plain)
+    this.cols = Sudoku.formatToCols(plain)
+    this.blocks = Sudoku.formatToBlocks(plain)
+  }
+
+  /**
+   * Format plain number sequence to rows
+   *
+   * Since the plain Sudoku sequence of numbers consists
+   * of the rows of the Sudoku written side by side, nine
+   * numbers next to each other are cut out and interpreted
+   * as a row.
+   * @param plain Plain number sequence of the sudoku.
+   * Basically all sudoku rows side by side.
+   * @returns Rows of sudoku as two-dimensional array
+   */
+  static formatToRows(plain: Unit): Unit[] {
+    // Create rows
+    const rows: Unit[] = []
+    // Slice always 9 numbers from plain array
+    for (let i = 0; i < 9; i += 1) {
+      rows.push(plain.slice(i * 9, i * 9 + 9))
+    }
+    return rows
+  }
+
+  /**
+   * Format plain number sequence to cols
+   *
+   * Creates nine columns with index c. It then fetches
+   * every ninth number plus the column index c and forms
+   * each column.
+   * @param plain Plain number sequence of the sudoku.
+   * Basically all sudoku rows side by side.
+   * @returns Cols of sudoku as two-dimensional array
+   */
+  static formatToCols(plain: Unit): Unit[] {
+    // Create cols
+    const cols: Unit[] = []
+    for (let colIndex = 0; colIndex < 9; colIndex += 1) {
+      const col: Unit = []
+      for (let rowIndex = 0; rowIndex < 9; rowIndex += 1) {
+        // Fetches every ninth number plus colIndex
+        col.push(plain[rowIndex * 9 + colIndex])
+      }
+
+      cols.push(col)
+    }
+    return cols
+  }
+
+  /**
+   * Format plain number sequence to blocks
+   *
+   * Takes every third element as the start index and adds
+   * 27 indices every three blocks. Now it cuts the first
+   * three numbers from the start index. He repeats this
+   * three times and adds nine to the starting index with
+   * each pass.
+   * @param plain Plain number sequence of the sudoku.
+   * Basically all sudoku rows side by side.
+   * @returns Blocks of sudoku as two-dimensional array
+   */
+  static formatToBlocks(plain: Unit): Unit[] {
+    const blocks: Unit[] = []
+    for (let blockIndex = 0; blockIndex < 9; blockIndex += 1) {
+      const blockStartIndex = blockIndex * 3 + Math.floor(blockIndex / 3) * 18
+
+      const block: Unit = [
+        ...plain.slice(blockStartIndex, blockStartIndex + 3),
+        ...plain.slice(blockStartIndex + 9, blockStartIndex + 9 + 3),
+        ...plain.slice(blockStartIndex + 18, blockStartIndex + 18 + 3),
+      ]
+
+      blocks.push(block)
+    }
+    return blocks
+  }
+}
+
+export default Sudoku
