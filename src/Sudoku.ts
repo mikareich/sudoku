05041eda2738d637970f87ad5516355861b02d4c
Mika Reich <mikareich0@gmail.com>
2022-01-04 19:26:44 +0100

    rename number to digit

--- src/Sudoku.ts
+++ src/Sudoku.ts
@@ -1,20 +1,39 @@
-export type Unit = (0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9)[]
+export type DigitSet = (1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9)[]
+
+export type DigitSetWithZero = (0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9)[]
 
 /** Interprets and splits sudoku */
 class Sudoku {
-  /** Possible numbers of a unit */
-  static NUMBER_SEQUENCE = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  /** Possible digits of a unit */
+  static DIGIT_SET: DigitSet = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  /** Format plain index to index of cell in all untis
+   * @param plainIndex Index of the cell in plain digit set
+   * @returns Index of the cell in all units
+   */
+  static plainIndexToUnitIndex(plainIndex: number) {
+    const rowIndex = Math.floor(plainIndex / 9)
+    const colIndex = plainIndex % 9
+    const blockIndex =
+      Math.floor(plainIndex / 27) * 3 + (Math.floor(plainIndex / 3) % 3)
+
+    return {
+      rowIndex,
+      colIndex,
+      blockIndex,
+    }
+  }
 
   /**
-   * Format plain number sequence to rows
-   * @param plain Plain number sequence of the sudoku.
+   * Format plain digit set to rows
+   * @param plain Plain digit set of the sudoku.
    * Basically all sudoku rows side by side.
    * @returns Rows of sudoku as two-dimensional array
    */
-  static formatToRows(plain: Unit): Unit[] {
+  static formatToRows(plain: DigitSetWithZero): DigitSetWithZero[] {
     // Create rows
-    const rows: Unit[] = []
-    // Slice always 9 numbers from plain array
+    const rows: DigitSetWithZero[] = []
+    // Slice always 9 digits from plain array
     for (let i = 0; i < 9; i += 1) {
       rows.push(plain.slice(i * 9, i * 9 + 9))
     }
@@ -22,18 +41,18 @@ class Sudoku {
   }
 
   /**
-   * Format plain number sequence to cols
-   * @param plain Plain number sequence of the sudoku.
+   * Format plain digit set to cols
+   * @param plain Plain digit set of the sudoku.
    * Basically all sudoku rows side by side.
    * @returns Cols of sudoku as two-dimensional array
    */
-  static formatToCols(plain: Unit): Unit[] {
+  static formatToCols(plain: DigitSetWithZero): DigitSetWithZero[] {
     // Create cols
-    const cols: Unit[] = []
+    const cols: DigitSetWithZero[] = []
     for (let colIndex = 0; colIndex < 9; colIndex += 1) {
-      const col: Unit = []
+      const col: DigitSetWithZero = []
       for (let rowIndex = 0; rowIndex < 9; rowIndex += 1) {
-        // Fetches every ninth number plus colIndex
+        // Fetches every ninth digit plus colIndex
         col.push(plain[rowIndex * 9 + colIndex])
       }
 
@@ -43,17 +62,17 @@ class Sudoku {
   }
 
   /**
-   * Format plain number sequence to blocks
-   * @param plain Plain number sequence of the sudoku.
+   * Format plain digit set to blocks
+   * @param plain Plain digit set of the sudoku.
    * Basically all sudoku rows side by side.
    * @returns Blocks of sudoku as two-dimensional array
    */
-  static formatToBlocks(plain: Unit): Unit[] {
-    const blocks: Unit[] = []
+  static formatToBlocks(plain: DigitSetWithZero): DigitSetWithZero[] {
+    const blocks: DigitSetWithZero[] = []
     for (let blockIndex = 0; blockIndex < 9; blockIndex += 1) {
       const blockStartIndex = blockIndex * 3 + Math.floor(blockIndex / 3) * 18
 
-      const block: Unit = [
+      const block: DigitSetWithZero = [
         ...plain.slice(blockStartIndex, blockStartIndex + 3),
         ...plain.slice(blockStartIndex + 9, blockStartIndex + 9 + 3),
         ...plain.slice(blockStartIndex + 18, blockStartIndex + 18 + 3),
@@ -64,23 +83,23 @@ class Sudoku {
     return blocks
   }
 
-  /** Plain number sequence of the sudoku. Basically all sudoku rows side by side. */
-  public plain: Unit = []
+  /** Plain digit set of the sudoku. Basically all sudoku rows side by side. */
+  public plain: DigitSetWithZero = []
 
   /** Rows of sudoku as two-dimensional array */
-  public rows: Unit[] = []
+  public rows: DigitSetWithZero[] = []
 
   /** Cols of sudoku as two-dimensional array */
-  public cols: Unit[] = []
+  public cols: DigitSetWithZero[] = []
 
   /** Blocks of sudoku as two-dimensional array */
-  public blocks: Unit[] = []
+  public blocks: DigitSetWithZero[] = []
 
   /**
    * Interprets and splits sudoku
-   * @param plain Plain number sequence of the sudoku. Basically all sudoku rows side by side.
+   * @param plain Plain digit set of the sudoku. Basically all sudoku rows side by side.
    */
-  constructor(plain: Unit) {
+  constructor(plain: DigitSetWithZero) {
     this.plain = plain
 
     this.rows = Sudoku.formatToRows(plain)
@@ -90,7 +109,7 @@ class Sudoku {
 
   /**
    * Returns units of the cell
-   * @param plainIndex Index of the cell in plain number sequence
+   * @param plainIndex Index of the cell in plain digit set
    * @returns The units of the cell
    */
   public getUnitsByPlainIndex(plainIndex: number) {
